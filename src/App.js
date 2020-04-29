d6e62c386b21355475cc69eee513843bbf66ec31
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-29 23:18:32 +0100

    Implementei as rotas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { useContext, useEffect } from 'react';
-import {AuthContext} from '../src/context/Auth'
 import {SocketContext} from '../src/context/SocketContext'
+import {BrowserRouter as Router} from 'react-router-dom'
+import MainRoutes from './routes'
 function App() {
   const {io} = useContext(SocketContext)
-  const {logado} = useContext(AuthContext);
 
   useEffect(()=> {
     io.emit('message',"ola mundo") 
   }
 ,[io])
   return (
-      <h1>Qualquer coisa</h1>
+      <Router>
+          <MainRoutes />
+      </Router>
   );
 }
 
